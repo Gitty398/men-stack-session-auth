e3b4c19302c1ddf76e9bb53b908b6ec32cc11f5c
John F <anabasisinc@outlook.com>
2026-01-12 14:40:51 -0500

    uploading completed auth template

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const router = express.Router();
+const user = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcrypt")
+
+
+router.get("/sign-up", (req, res) => {
+    res.render("auth/sign-up.ejs")
+})
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs")
+})
+
+router.post("/sign-in", async (req, res) => {
+    const userInDB = await User.findOne({ username: req.body.username })
+    if (!userInDB) {
+        return res.send(`A user with username ${req.body.username} does not exist.`)
+    }
+    const isValidPassword = bcrypt.compareSync(req.body.password, userInDB.password)
+    if (!isValidPassword) {
+        return res.send("Password Incorrect")
+    }
+
+
+    // here too-----------------------------------
+
+    req.session.user = {
+        username: userInDB.username,
+        _id: userInDB._id,
+    }
+
+
+    // here------------------------------------------
+
+    req.session.save(() => {
+        res.redirect("/")
+    })
+})
+
+router.get("/sign-out", (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
+})
+
+// here ---------------------------------------------
+
+
+
+router.post("/sign-up", async (req, res) => {
+    const userInDB = await User.findOne({ username: req.body.username })
+    if (userInDB) {
+        return res.send(`A user with username ${req.body.username} already exists`)
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send(`Your password and confirm password must match!`)
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword
+    const newUser = await User.create(req.body)
+
+    res.send(newUser)
+})
+
+
+module.exports = router;
